87d8dcec252e5a22a861d4badc40247425eccf25
gary <gary.meliman@outlook.com>
2016-03-13 11:30:55 -0300

    some

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     get 'wp_admin_login' => 'devise/sessions#new', :as => :new_user_session
     post 'wp_admin_login' => 'devise/sessions#create', :as => :user_session
     delete 'wp_admin_logout' => 'devise/sessions#destroy', :as => :destroy_user_session
+    get 'wp_admin/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'wp_admin' => 'devise/registrations#update', :as => 'user_registration'
   end
   get 'admin_dashboard/index'
 
